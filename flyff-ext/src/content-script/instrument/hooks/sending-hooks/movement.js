f48a0048ba22560b5e27713beedd1f6a873463e9
Jacob Sifuentes <jacob.sifuentes@skillshare.com>
2024-01-13 17:47:33 -0500

    updates for movement packet reading

--- flyff-ext/src/content-script/instrument/hooks/sending-hooks/movement.js
+++ flyff-ext/src/content-script/instrument/hooks/sending-hooks/movement.js
@@ -1,13 +1,27 @@
 export default function handleMovementPacket(message) {
-    wyff.logger.info(`got movement packet: ${JSON.stringify(message)}`);
-    // const view = new DataView(message.buffer);
-    // // skip first four bytes
-    // const offset = 4;
-    // const unknownBytes = 16 + 2; // 2 for 2 null bytes
-    // // const unknownStuff = view.getUint32(16 + offset, true);
-    // const x = view.getFloat32(unknownBytes + offset, true);
-    // const y = view.getFloat32(unknownBytes + offset + 4 + 4 /** null bytes */, true);
-    // // const z = view.getFloat32(unknownBytes + offset + 4 + 4 /** null bytes */ + 4 + 4 /** null bytes */, true);
-    // // const angle = view.getFloat32(unknownBytes + offset + 4 + 4 /** null bytes */ + 4 + 4 /** null bytes */ + 4 + 4 /** null bytes */, true);
-    
+    const view = new DataView(message.buffer);
+
+    const cmdPacketLength = 4;
+    const unknownBytes = 13;
+    const movementOperationOffset = cmdPacketLength + unknownBytes;
+
+    const movementOperation = view.getUint8(movementOperationOffset);
+
+    wyff.logger.info(`movement operation: ${movementOperation}`);
+
+    const startOfMovementOperationArguments = cmdPacketLength + 18;
+    if (movementOperation === 1) {
+        // we are moving in a direction using movement keys
+        const angleOffset = startOfMovementOperationArguments;
+        const angle = view.getFloat32(angleOffset, true);
+
+        wyff.logger.info(`moving using movement keys: angle: ${angle}`);
+    } else if (movementOperation === 7) {
+        // we are moving to a specific location using the mouse
+        const x = view.getFloat32(startOfMovementOperationArguments, true);
+        const y = view.getFloat32(startOfMovementOperationArguments + 8, true);
+        const z = view.getFloat32(startOfMovementOperationArguments + 16, true);
+
+        wyff.logger.info(`moving using the mouse: x: ${x}, y: ${y}, z: ${z}`);
+    }
 }
